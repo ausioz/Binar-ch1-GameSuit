7e26e228a727731e2d3a3a2f55895c02bc91c244
ausioz <oz.ausi@gmail.com>
2022-07-01 21:09:33 +0700

    Adding AI game mode

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,13 +1,33 @@
+import kotlin.random.Random
+
 class Main {
 }
 fun main (){
     val gameBanner =Banner()
     gameBanner.createGameBanner()
     val ruleGameSuit = RuleSuit()
-    val pemain1Input:String? = readLine()
-    val pemain21Input:String? = readLine()
-    ruleGameSuit.rule(pemain1Input,pemain21Input)
-    ruleGameSuit.result()
+    val gameMode: Int = readLine()?.toInt() ?: 1
+
+    if (gameMode == 1) {
+        val pemain1Input: String? = readLine()
+        val pemainAIInput: String = RuleSuit().pemainAI
+        ruleGameSuit.rule(pemain1Input, pemainAIInput)
+        println(pemainAIInput)
+        ruleGameSuit.result()
+    }
+    else if (gameMode == 2) {
+        val pemain1Input: String? = readLine()
+        val pemain2Input: String? = readLine()
+        ruleGameSuit.rule(pemain1Input, pemain2Input)
+        ruleGameSuit.result()
+    }
+    else {
+        println("Command Error!")
+        println("Masukan game mode (1/2)\n")
+        return main()
+    }
+
+
 }
 
 
--- src/main/kotlin/RuleSuit.kt
+++ src/main/kotlin/RuleSuit.kt
@@ -1,21 +1,23 @@
-class RuleSuit {
-    val batu: String = "Batu"
-    val gunting: String = "Gunting"
-    val kertas: String = "Kertas"
+import kotlin.random.Random
+
+open class RuleSuit {
+    val pilihanSuit = arrayOf("Batu","Gunting","Kertas")
     var status: String? = null
     fun rule(pemain1:String?, pemain2:String?){
+        // 0: Batu      1: Gunting      2:Kertas
         if (
-            pemain1 == batu && pemain2 == gunting ||
-            pemain1 == gunting && pemain2 == kertas ||
-            pemain1 == kertas && pemain2 == batu)
+            pemain1 == pilihanSuit[0] && pemain2 == pilihanSuit[1] ||
+            pemain1 == pilihanSuit[1] && pemain2 == pilihanSuit[2] ||
+            pemain1 == pilihanSuit[2] && pemain2 == pilihanSuit[0])
             { status = "Pemain 1 MENANG!" }
         else if (
-            pemain1 == batu && pemain2 == kertas ||
-            pemain1 == gunting && pemain2 == batu ||
-            pemain1 == kertas && pemain2 == gunting)
+            pemain1 == pilihanSuit[0] && pemain2 == pilihanSuit[2] ||
+            pemain1 == pilihanSuit[1] && pemain2 == pilihanSuit[0]||
+            pemain1 == pilihanSuit[2] && pemain2 == pilihanSuit[1])
             { status = "Pemain 2 MENANG!" }
         else { status = "DRAW!"  }
     }
+    val pemainAI = pilihanSuit.random()
     fun result(){
         println("\u001B[1m"+status)
     }
